0e5e7608433eaf5d5eb636266b0f59ea0e471524
Hein Htet <heinhtet.hh405@gmail.com>
2017-09-20 11:22:40 +0630

    Login

--- login.php
+++ login.php
@@ -1,8 +1,31 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'include/DB_Functions.php';
+$db = new DB_Functions();
 
+$response = array("error" => FALSE);
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = $db->getUserByEmailAndPassword($email, $password);
+
+    if ($user != false) {
+        $response["error"] = FALSE;
+        $response["uid"] = $user["unique_id"];
+        $response["user"]["name"] = $user["name"];
+        $response["user"]["email"] = $user["email"];
+        $response["user"]["created_at"] = $user["created_at"];
+        $response["user"]["updated_at"] = $user["updated_at"];
+        echo json_encode($response);
+    } else {
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Login credentials are wrong. Please try again!";
+        echo json_encode($response);
+    }
+} else {
+    $response["error"] = TRUE;
+    $response["error_msg"] = "Required parameters email or password is missing!";
+    echo json_encode($response);
+}
